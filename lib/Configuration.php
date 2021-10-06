7d509f3d73bddb058ddb454ee7f5e83525bc66df
cardpay-ci <rnd@cardpay.com>
2021-10-06 08:37:19 +0000

    Published latest changes

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -59,7 +59,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'CardpaySdk/3.8.0.1/PHP';
+    protected $userAgent = 'CardpaySdk/3.8.8/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -371,7 +371,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 3.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 3.8.0.1' . PHP_EOL;
+        $report .= '    SDK Package Version: 3.8.8' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
